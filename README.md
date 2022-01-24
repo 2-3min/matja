99bbd0908d80a22b31338fe3f5f56bf02468ced9
2-3min <foreverything1023@gmail.com>
2022-01-24 22:58:28 +0900

    layout_flexbox

---
+++ README.md
@@ -0,0 +1,1 @@
+My first toy project is "matja"
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="container">
+    <div className="contents l-contents">
+      <div className="l-contents__matZip">matzipList</div>
+    </div>
+    <div className="contents r-contents">
+      <div className="r-contents__userList">userList</div>
+      <div className="r-contents__map">map</div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
